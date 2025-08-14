1fb12b3044bf953cc8fa395565c93fae5b1b145f
Ezequiel Bugnon <bugnonezequiel@gmail.com>
2025-08-14 20:06:07 -0300

    feat(app): add s3 folder

---
+++ terraform-lab-aws/s3/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "demo-bucket-${random_id.suffix.hex}"
+
+  tags = {
+    Name = "demo-bucket"
+  }
+}
